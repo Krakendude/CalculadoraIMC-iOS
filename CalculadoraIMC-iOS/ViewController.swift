9ad4c3e120c8a218619886dc7a4f3e384ae44003
ramiro <radelacruzve@gmail.com>
2025-06-09 16:59:47 +0200

    haiii

--- CalculadoraIMC-iOS/ViewController.swift
+++ CalculadoraIMC-iOS/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    var weight: Float = 70
+    var height: Float = 160
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func onWeightChanged(_ sender: UIStepper) {
+        weight = Float(sender.value)
+        weightLabel.text = "\(Int(weight)) kg"
+    }
+    
+    @IBAction func onHeightChanged(_ sender: UISlider) {
+        height = sender.value
+        heightLabel.text = "\(Int(height)) cm"
+    }
+    
+    @IBAction func calculateButton(_ sender: Any) {
+        let heightInMeter: Float = height / Float(100)
+                let bmi: Float = weight / pow(heightInMeter, 2)
+                resultLabel.text = "\(String(format: "%.2f", bmi))"
+            }
+    
 }
 
